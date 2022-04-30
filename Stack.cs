6bca1cdda99944c3fe059301352b422da69f62fb
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-30 16:36:12 -0500

    completed exercise

--- Stack.cs
+++ Stack.cs
@@ -1,20 +1,44 @@
-﻿namespace InheritanceExercise
+﻿using System.Collections;
+
+namespace InheritanceExercise
 {
     public class Stack
     {
+        public ArrayList List { get; }
+        public Stack()
+        {
+            List = new ArrayList();
+        }
         public void Push(object obj)
         {
-
+            if (obj == null)
+            {
+                throw new System.InvalidOperationException("Should not contain a null object.");
+            }
+            else
+            {
+                List.Add(obj);
+            }
+            
         }
 
         public object Pop()
         {
-            return new object();
+            if (List.Count == 0)
+            {
+                throw new System.InvalidOperationException("This ArrayList is empty.");
+            }
+            else
+            {
+                var popItem = List[List.Count - 1];
+                List.Remove(List[List.Count - 1]);
+                return popItem;
+            }
         }
 
         public void Clear()
         {
-
+            List.Clear();
         }
 
     }
